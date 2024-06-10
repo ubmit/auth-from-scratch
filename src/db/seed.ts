c5a09a524bcde0b543edee57eaa181b72f16542e
Guilherme de Andrade <gui@ubmit.dev>
2024-06-10 16:38:58 +0100

    fix: add hashed password to seed data

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,22 +1,26 @@
+import { password } from 'bun'
 import { db } from '.'
 
-const USERS = [
-  { $username: 'pat', $password: '123' },
-  { $username: 'gui', $password: '456' },
-]
+async function getInitialUsers() {
+  return [
+    { $username: 'pat', $password: await password.hash('123') },
+    { $username: 'gui', $password: await password.hash('456') },
+  ]
+}
 
-db.run(
+db.exec(
   'CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)',
 )
 
-const insertUser = db.prepare(
-  'INSERT INTO users (username, password) VALUES ($username, $password)',
-)
+getInitialUsers().then((users) => {
+  const insertUser = db.prepare(
+    'INSERT INTO users (username, password) VALUES ($username, $password)',
+  )
+  const insertUsers = db.transaction((users) => {
+    for (const user of users) {
+      insertUser.run(user)
+    }
+  })
 
-const insertUsers = db.transaction((users) => {
-  for (const user of users) {
-    insertUser.run(user.$username, user.$password)
-  }
+  insertUsers(users)
 })
-
-insertUsers(USERS)
