1d58687bd9667ac4e59fdb1c392e2533bdce369e
Guilherme de Andrade <gui@ubmit.dev>
2024-06-10 02:38:17 +0100

    refactor: use Bun.sleep

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,9 @@
 import { Hono } from "hono";
 import { methodOverride } from "hono/method-override";
+import { sleep } from "bun";
 
 import { Home } from "./pages/home";
 import { Login } from "./pages/login";
-
-function sleep(ms: number) {
-  return new Promise((r) => setTimeout(r, ms));
-}
 
 const app = new Hono();
 
@@ -24,9 +21,10 @@ app.use("/session", methodOverride({ app, query: "_method" }));
 
 app.post("/session", async (c) => {
   const fd = await c.req.formData();
+  console.log(fd);
   console.log({ username: fd.get("username"), password: fd.get("password") });
   console.log("Logging in...");
-  await sleep(1000);
+  sleep(1000);
   isLoggedIn = true;
   console.log(`Logged in as ${fd.get("username")}`);
   return c.redirect("/");
@@ -34,7 +32,7 @@ app.post("/session", async (c) => {
 
 app.delete("/session", async (c) => {
   console.log("Logging out...");
-  await sleep(1000);
+  sleep(1000);
   isLoggedIn = false;
   console.log("User is now logged out");
   return c.redirect("/");
